5d12a101631670d06fc1fe5a92571d1272628d52
Gayathiri Muthusamy <gayathirimuthusamy73@gmail.com>
2024-05-29 14:05:22 +0530

    Update ASCII value of Single Character.py

--- STRING/ASCII value of Single Character.py
+++ STRING/ASCII value of Single Character.py
@@ -1,6 +1,7 @@
 my_string=input("Enter the value:")
 print(ord(my_string))
 
+#using def function
 def single_character(my_string):
     print(ord(my_string))
 single_character(input("Enter the value :"))
