3ccfb55e11d7a4215c1c212f1ae406a967e1e180
Gayathiri Muthusamy <gayathirimuthusamy73@gmail.com>
2024-05-29 14:06:37 +0530

    Update ASCII value of total characters.py

--- STRING/ASCII value of total characters.py
+++ STRING/ASCII value of total characters.py
@@ -1,10 +1,11 @@
-# my_string=input("Enter the value :")
-# total=0
-# for i in my_string:
-    # print(f"{i}:{ord(i)}")
-    # total=total+ord(i)
-#print(total)
+my_string=input("Enter the value :")
+total=0
+for i in my_string:
+    print(f"{i}:{ord(i)}")
+    total=total+ord(i)
+print(total)
 
+#using function
 def characters(my_string):
     total=0
     for i in my_string:
