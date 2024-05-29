6457a2339d8948ad74cad16141aa316622e3ad01
Gayathiri Muthusamy <gayathirimuthusamy73@gmail.com>
2024-05-29 14:08:02 +0530

    Update Concatenate Strings.py

--- STRING/Concatenate Strings.py
+++ STRING/Concatenate Strings.py
@@ -1,9 +1,9 @@
-# my_string_1="I Like"
-# my_string_2="PYTHON"
+my_string_1="I Like"
+my_string_2="PYTHON"
+result=my_string_1+' '+my_string_2
+print(result)
 
-# result=my_string_1+' '+my_string_2
-# print(result)
-
+#using function
 def concatenate(my_string_1,my_string_2):
     result=my_string_1+' '+my_string_2
     print(result)
